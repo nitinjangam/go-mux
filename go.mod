dbce5e62df588cdc7bc4cec3bfcc55d7d80b26f7
Nitin Jangam <njangam169@gmail.com>
2021-04-17 00:33:18 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nitinjangam/go-mux
+
+go 1.14
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.0
+)
---
+++ main_test.go
@@ -0,0 +1,43 @@
+package main_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/nitinjangam/go-mux/main"
+)
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
+
+var a main.App
+
+func TestMain(m *testing.M) {
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"),
+	)
+
+	ensureYableExists()
+	code := m.Run()
+	clearTable()
+	os.Exit(code)
+}
+
+func ensureYableExists() {
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func clearTable() {
+	a.DB.Exec("DELETE FROM products;")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1;")
+}
